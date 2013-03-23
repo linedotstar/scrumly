056cf3d7c6cf13d07a7729d179dc75d962e6eea1
linedotstar <linedotstar@gmail.com>
2013-03-23 10:30:17 -0700

    rails_apps_composer: initial commit

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Scrumly::Application.config.secret_token = '853604c85cebea96c74e30000960b438b2e8a4625cb09ed6a1c40a617c9178c42465087a7117ceffdcaa186623da1faf22431d8edbc211c80dd6ee1caa4ea7cc'
+Scrumly::Application.config.secret_token = '3ad65cc9e35fb39aa67f712dbbd8587d04c18b0ba97cba8faf985c75995057b74aa004ee156bee13c951680ddf9a71b99d8f022c66cbf1fd80a8418df1b17c96'
